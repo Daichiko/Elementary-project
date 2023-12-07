49730052a249d5850ea834659a8290847f5213c0
Manuel Aguilar <56054393+Daichiko@users.noreply.github.com>
2023-12-07 17:25:48 -0400

    Feat[ELE-30-43-44-69]/multiples funcionalidades
    Se agrega interaccion con la barra de vida y barra de experiencia en el hud, se agrega animaciones basicas al player, se agrega la posibilidad de seleccionar al nuevo player en el menu

--- Elementary/Assets/Scripts/PlayerS/Player1(fire)/FirePlayerAttack.cs
+++ Elementary/Assets/Scripts/PlayerS/Player1(fire)/FirePlayerAttack.cs
@@ -4,18 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class FirePlayerAttack : MonoBehaviour
+public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private Transform controladorGolpe;
     [SerializeField] private float radioGolpe;
-    [SerializeField] private float dañoGolpe;
+    [SerializeField] private int[] dañoGolpe;
     [SerializeField] private float[] tiempoEntreAtaques;
     [SerializeField] private float[] tiempoSiguienteAtaque;
 
+    [SerializeField] private int level;
+    [SerializeField] private int experience;
+    [SerializeField] private int maxExperience;
     [SerializeField] private int Healt;
     [SerializeField] private int maxHealt;
     [SerializeField] private int bonusResistence;
     [SerializeField] private int bonusDamage;
+
+
+    public HealtBarScript healthbar;
+    public ExperienceBarScript experiencebar;
 
     private Animator animator;
 
@@ -23,6 +30,14 @@ public class FirePlayerAttack : MonoBehaviour
     private void Start()
     {
         animator = GetComponent<Animator>();
+        GameObject healthbarObject = GameObject.FindWithTag("healthbar");
+        GameObject ExperiencebarObject = GameObject.FindWithTag("Experiencebar");
+        healthbar = healthbarObject.GetComponent<HealtBarScript>();
+        experiencebar = ExperiencebarObject.GetComponent<ExperienceBarScript>();
+        Healt = maxHealt;
+        healthbar.SetMaxHealth(maxHealt);
+        experiencebar.SetMaxXP(maxExperience);
+        
     }
 
 
@@ -43,9 +58,8 @@ public class FirePlayerAttack : MonoBehaviour
         {
             if (Input.GetButtonDown("Attack1"))
             {
-                print("entre1");
                 animator.SetTrigger("Attack1");
-                Golpe();
+                Golpe(dañoGolpe[0]);
                 tiempoSiguienteAtaque[0] = tiempoEntreAtaques[0];
             }
         }
@@ -54,9 +68,8 @@ public class FirePlayerAttack : MonoBehaviour
         {
             if (Input.GetButtonDown("Attack2"))
             {
-                print("entre2");
                 animator.SetTrigger("Attack2");
-                Golpe();
+                Golpe(dañoGolpe[1]);
                 tiempoSiguienteAtaque[1] = tiempoEntreAtaques[1];
             }
         }
@@ -65,15 +78,24 @@ public class FirePlayerAttack : MonoBehaviour
         {
             if (Input.GetButtonDown("Attack3"))
             {
-                print("entre3");
                 animator.SetTrigger("Attack3");
-                Golpe();
+                Golpe(dañoGolpe[2]);
                 tiempoSiguienteAtaque[2] = tiempoEntreAtaques[2];
             }
-        }      
+        }
+
+        if (Input.GetButtonDown("lost health")) 
+        {
+            takeDamage(50);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            gainXP(30);
+        }
     }
 
-    private void Golpe()
+    private void Golpe(int damage)
     {
         Collider2D[] objetos = Physics2D.OverlapCircleAll(controladorGolpe.position, radioGolpe);
 
@@ -81,7 +103,7 @@ public class FirePlayerAttack : MonoBehaviour
         {
             if (colisionador.CompareTag("Enemy")) 
             {
-                colisionador.transform.GetComponent<Enemy>().TomarDaño(dañoGolpe);
+                colisionador.transform.GetComponent<Enemy>().TomarDaño(damage);
             }
         }
     }
@@ -90,5 +112,42 @@ public class FirePlayerAttack : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(controladorGolpe.position, radioGolpe);
+    }
+
+    public void takeDamage(int damage) 
+    {
+        Healt -= damage;
+        healthbar.SetHealth(Healt);
+
+        if (Healt <- 0f) 
+        {
+            healthbar.SetHealth(0);
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void lvlUP()
+    {
+        maxHealt = (int)Math.Truncate(1.2 * maxHealt);
+        maxExperience = (int)Math.Truncate(1.2 * maxExperience);
+        dañoGolpe[0] = (int)Math.Truncate(1.2 * dañoGolpe[0]);
+        dañoGolpe[1] = (int)Math.Truncate(1.2 * dañoGolpe[1]);
+        dañoGolpe[2] = (int)Math.Truncate(1.2 * dañoGolpe[2]);
+        print(dañoGolpe);
+        level += 1;
+        experience = 0;
+        Healt = maxHealt;
+    }
+
+    public void gainXP(int xp) 
+    {
+        experience += xp;
+        experiencebar.SetXP(experience);
+        if (experience >= maxExperience)
+        {  
+            lvlUP();
+            experiencebar.SetMaxXP(maxExperience);
+            healthbar.SetMaxHealth(maxHealt);
+        };
     }
 }
